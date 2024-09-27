80e078056d47675366c85c11c2cccf1d60e7d250
Vivian Davila <168599473+viviancreates@users.noreply.github.com>
2024-09-27 18:42:22 +0000

    Adding to director template details

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,5 @@ Rails.application.routes.draw do
 
   get("/directors", { :controller => "directors", :action => "index"})
 
-  get("/directors/:an_id", { :controller => "directors", :action => "show"})
+  get("/directors/:the_id", { :controller => "directors", :action => "show"})
 end
